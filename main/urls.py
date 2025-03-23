bfa993e0024c440fac159c229b4e42d81efc9a92
Ojo-Oluwagbenga <ojojohn2907@gmail.com>
2025-03-23 21:09:43 +0100

    General Commit

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [    
+    path("login", views.login, name="login"),
+    
+    path("dashboard", views.dashboard, name="signup"),
+    path("report/<str:report_code>", views.report, name="report"),
+
+    path("logout", views.logout, name="signup"),
+    path("", views.homepage, name="homepage"),
+
+]
---
+++ main/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+# Create your views here.
+
+def only_logged_outs(v_method):
+    def wrap(response):
+        if response.session.get("user_data"):
+            return redirect("/dashboard")
+        else:
+            return v_method(response)
+    return wrap
+
+@only_logged_outs
+def login(response):
+    return render(response, "login.html", {})
+
+
+def dashboard(response):
+    if not response.session.get("user_data"):
+        response.session.flush()
+        return redirect("/login")
+            
+    #UPDATE THE USER KEY EXPIRY SINCE USER IS ACTIVE
+    response.session.set_expiry(864000) #SHIFTS TO 10 DAYS FROM NOW
+    
+    qset = {
+        "user_data":response.session['user_data'],
+        "name":response.session['user_data']['user_name']
+    }
+
+    return render(response, "dashboard.html", qset)
+
+def homepage(response):
+    return render(response, "homepage.html", {})
+
+def report(response, report_code):
+    if not response.session.get("user_data"):
+        response.session.flush()
+        return redirect("/login")
+            
+    #UPDATE THE USER KEY EXPIRY SINCE USER IS ACTIVE
+    response.session.set_expiry(864000) #SHIFTS TO 10 DAYS FROM NOW
+    user_code = response.session['user_data'].get("user_code")
+    
+    qset = {
+        'draft':"Here is the draft",
+        'login_type':'-'
+    }
+
+    return render(response, "report.html", qset)
+
+
+def logout(response):
+    response.session.flush()
+    return redirect("/login")
